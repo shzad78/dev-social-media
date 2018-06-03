61d6b03181483a5ff9f6ffe4737eafe844de6b92
shzdad78 <shzad78@gmail.com>
2018-06-03 16:24:23 +0500

    profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
 
 // @route   GET api/profile/test
 // @desc    Tests profile route
 // @access  Public
-router.get('/test', (req, res) => res.json({ msg: 'Profile Works' }));
+
+router.get("/test", (req, res) => res.json({ msg: "Profile Works" }));
+
+router.get("/", passport.authenticate("jwt", {}));
 
 module.exports = router;

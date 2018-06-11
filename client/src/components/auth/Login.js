98cd39660882a95aa4f5ad760b10dd3597b42be6
shzdad78 <shzad78@gmail.com>
2018-06-11 14:26:17 +0500

    Router

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+export default class Register extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Register</h1>
+      </div>
+    );
+  }
+}
+import React, { Component } from 'react'
+
+class Login extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Login</h1>
+      </div>
+    )
+  }
+}
+export default Login;

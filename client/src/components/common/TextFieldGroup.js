6ae3e652279f36db0c61d4bfbd0847f0508b0bc7
shahzad babar <shzad78@gmail.com>
2018-06-26 22:54:41 +0500

    login

--- client/src/components/common/TextFieldGroup.js
+++ client/src/components/common/TextFieldGroup.js
@@ -3,19 +3,18 @@ import classnames from 'classnames';
 import PropTypes from 'prop-types';
 
 const TextFieldGroup = ({
-    name,
-    placeholder,
-    value,
-    label,
-    error,
-    info,
-    type,
-    onChange,
-    disabled
-  }) => {
+  name,
+  placeholder,
+  value,
+  label,
+  error,
+  info,
+  type,
+  onChange,
+  disabled
+}) => {
   return (
-    <div>
-      className="form-group">
+    <div className="form-group">
       <input
         type={type}
         className={classnames('form-control form-control-lg', {
@@ -30,21 +29,22 @@ const TextFieldGroup = ({
       {info && <small className="form-text text-muted">{info}</small>}
       {error && <div className="invalid-feedback">{error}</div>}
     </div>
-  )
-}
+  );
+};
+
 TextFieldGroup.propTypes = {
-    name: PropTypes.string.isRequired,
-    placeholder: PropTypes.string,
-    value: PropTypes.string.isRequired,
-    info: PropTypes.string,
-    error: PropTypes.string,
-    type: PropTypes.string.isRequired,
-    onChange: PropTypes.func.isRequired,
-    disabled: PropTypes.string
-  };
-  
-  TextFieldGroup.defaultProps = {
-    type: 'text'
-  };
-  
-  export default TextFieldGroup;
+  name: PropTypes.string.isRequired,
+  placeholder: PropTypes.string,
+  value: PropTypes.string.isRequired,
+  info: PropTypes.string,
+  error: PropTypes.string,
+  type: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  disabled: PropTypes.string
+};
+
+TextFieldGroup.defaultProps = {
+  type: 'text'
+};
+
+export default TextFieldGroup;

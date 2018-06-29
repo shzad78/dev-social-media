e3b97008dcb32d1c8cdc1f37dfede078f3215c7b
shahzad babar <shzad78@gmail.com>
2018-06-29 18:44:53 +0500

    CreateProfile

---
+++ client/src/components/create-profile/CreateProfile.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import PropTypes from 'proptypes';
+
+ class CreateProfile extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+export default connect(mapStateToProps)(CreateProfile)

b10b07331e471c08acc5f20bb030949fc5d42440
shahzad babar <shzad78@gmail.com>
2018-07-03 13:53:43 +0500

    Add education

---
+++ client/src/components/add-credentials/AddEducation.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import {Link, Router} from 'react-router-dom';
+import {connect} from 'react-redux';
+import {PropTypes} from 'prop-types';
+ 
+
+
+export default class AddEducation extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
---
+++ client/src/components/dashboard/Education.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
+import {withRouter} from ' react-router-dom';
+
+ class Education extends Component {
+  render() {
+      const experience = this.props.experience.map(exp => (
+        <tr></tr>
+
+      ))
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+export default connect();(Education)
